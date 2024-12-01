b4d4dfd2fb46be63c629dcd560e8a022fe094ae1
TaYWEe <patryk.wenz@gmail.com>
2024-12-01 19:28:10 +0100

    Addition: Cards in blog

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,52 +21,48 @@ const Image = styled.img`
 	margin-left: 15rem;
 	flex-direction: column;
 	display: flex;
-
 `;
 const About = () => {
 	return (
 		<motion.h1
-		initial={{ opacity: 0, scale: 0.5 }}  
-		animate={{ opacity: 1, scale: 1 }}  
-		transition={{ duration: 0.5 }}     
-	  >
-		<Section className="about-section">
-			<TextContainer className="about-text-container">
-
-
-
-				<h2>Hi, my name is</h2>
-				<h1>Patryk Wenz</h1>
-				<h3>
-					I am a software engineer with a passion for developing
-					innovative solutions. I am currently researching vector
-					databases, including
-				</h3>
-				<h3 className="about-text-container-special">Qdrant and Streamlit.</h3>
-				<div className="about-social-links">
-					<a
-						target="_blank"
-						rel="noopener noreferrer"
-						href="https://github.com/PatrykWenz-4"
-						aria-label="GitHub profile link"
-					>
-						<FaGithub />
-					</a>
-					<a
-						target="_blank"
-						rel="noopener noreferrer"
-						href="https://www.linkedin.com/in/patryk-wenz-37710a1a8/"
-						aria-label="LinkedIn profile link"
-					>
-						<FaLinkedin />
-					</a>
-				</div>
-			</TextContainer>
-			<Image src={Face} alt="Blog Visual" />
-			
-		</Section>
+			initial={{ opacity: 0, scale: 0.5 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{ duration: 0.5 }}
+		>
+			<Section className="about-section">
+				<TextContainer className="about-text-container">
+					<h2>Hi, my name is</h2>
+					<h1>Patryk Wenz</h1>
+					<h3>
+						I am a software engineer with a passion for developing
+						innovative solutions. I am currently researching vector
+						databases, including
+					</h3>
+					<h3 className="about-text-container-special">
+						Qdrant and Streamlit.
+					</h3>
+					<div className="about-social-links">
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://github.com/PatrykWenz-4"
+							aria-label="GitHub profile link"
+						>
+							<FaGithub />
+						</a>
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://www.linkedin.com/in/patryk-wenz-37710a1a8/"
+							aria-label="LinkedIn profile link"
+						>
+							<FaLinkedin />
+						</a>
+					</div>
+				</TextContainer>
+				<Image src={Face} alt="Blog Visual" />
+			</Section>
 		</motion.h1>
-
 	);
 };
 
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,59 +1,55 @@
 import React from "react";
 import docker from "./docker.png";
-import styled from "styled-components";
+import springOllama from "./springOllama.png";
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion"
 
-const BlogWrapper = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 2rem;
-	@media (max-width: 768px) {
-		flex-direction: column;
-		align-items: flex-start;
-	}
-`;
-
-const TextWrapper = styled.div`
-	max-width: 60%;
-	margin-left: 1.5rem;
-`;
-
-const ImageLink = styled(Link)`
-	display: inline-block;
-	width: 20vw;
-	height: auto;
-	margin-left: 1.5rem;
-	text-decoration: none;
-
-	@media (max-width: 768px) {
-		width: 100%;
-		margin-left: 0;
-	}
-`;
-
-const Image = styled.img`
-	width: 100%;
-	height: auto;
-	border-radius: 80px;
-	transition: transform 0.3s ease, border-radius 0.3s ease;
-
-	&:hover {
-		transform: scale(1.05);
-		border-radius: 20px;
-	}
-`;
+import CardGroup from "react-bootstrap/CardGroup";
 
 const Blog = () => (
-	<BlogWrapper>
-		<TextWrapper>
-			<h1>First Steps to Understanding Docker</h1>
-		</TextWrapper>
-		<ImageLink to="/Portfolio/Blog/docker-details">
-			<Image src={docker} alt="Blog Visual" />
-		</ImageLink>
-	</BlogWrapper>
+	<CardGroup className="gap-3">
+		<Card style={{ width: "18rem" }}>
+			<Card.Img variant="top" src={docker} />
+			<Card.Header></Card.Header>
+			<Card.Body>
+				<Card.Title>
+					Basic Docker Setup Using SQL and phpMyAdmin Images as an
+					Example. Practical usage.
+				</Card.Title>
+				<Card.Text></Card.Text>
+				<Button
+					variant="primary"
+					as={Link}
+					to="/Portfolio/Blog/docker-details"
+				>
+					Go to the article
+				</Button>
+			</Card.Body>
+		</Card>
+
+		<Card style={{ width: "18rem" }}>
+			<Card.Img variant="top" src={springOllama} />
+			<Card.Header></Card.Header>
+
+			<Card.Body>
+				<Card.Title>
+					Basic usage of Ollama models with spring boot AI.
+				</Card.Title>
+				<Card.Text></Card.Text>
+				<Button
+					variant="primary"
+					as={Link}
+					to="/Portfolio/Blog/docker-details"
+				>
+					Go to the article
+				</Button>
+			</Card.Body>
+		</Card>
+	</CardGroup>
 );
 
 export default Blog;
